a3f7723aa3f257f9470498dac0050dfdc77d4736
doulosjj1992-debug <doulosjj1992@gmail.com>
2025-09-27 19:46:15 -0500

    Add Pages router homepage (iframe to landing-proto.html)

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <iframe
+      src="/landing-proto.html"
+      style={{ border: '0', width: '100vw', height: '100vh' }}
+      title="The Star Auto Service"
+    />
+  );
+}
