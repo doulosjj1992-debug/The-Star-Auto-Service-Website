2af5a09472ffcbd6a78b7e86a197ab58aa1ddf01
doulosjj1992-debug <doulosjj1992@gmail.com>
2025-09-27 19:50:21 -0500

    Remove pages/ to resolve router conflict (use App Router at /)

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <iframe
-      src="/landing-proto.html"
-      style={{ border: '0', width: '100vw', height: '100vh' }}
-      title="The Star Auto Service"
-    />
-  );
-}
